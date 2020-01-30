46d6e740c0a9c5043785738805c65d5bd964019d
norvca <norvca@gmail.com>
2020-01-30 14:01:38 +0800

    Write model layer

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+const colors = require('colors');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 // Router files
 const bootcamps = require('./routes/bootcamps');
 
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -19,6 +27,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode, port: ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode, port: ${PORT}`.yellow
+  );
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err}`.red);
+  server.close(() => process.exit(1));
 });
