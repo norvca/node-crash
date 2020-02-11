1a22c4ca51bb8d35836589c103de05a9dd146b24
norvca <norvca@gmail.com>
2020-02-11 19:56:27 +0800

    Add course ownership

--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ const UserSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['user', 'publisher'],
+    enum: ['admin', 'user', 'publisher'],
     default: 'user'
   },
   password: {
